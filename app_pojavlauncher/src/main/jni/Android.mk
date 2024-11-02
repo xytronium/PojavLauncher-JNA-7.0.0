5340319c99a01741cf0e2ed8141d0387de106822
xytronium <100645830+xytronium@users.noreply.github.com>
2024-11-02 11:11:03 -0400

    Add files via upload

--- app_pojavlauncher/src/main/jni/Android.mk
+++ app_pojavlauncher/src/main/jni/Android.mk
@@ -8,27 +8,12 @@ HERE_PATH := $(LOCAL_PATH)
 
 LOCAL_PATH := $(HERE_PATH)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := angle_gles2
-LOCAL_SRC_FILES := tinywrapper/angle-gles/$(TARGET_ARCH_ABI)/libGLESv2_angle.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := tinywrapper
-LOCAL_SHARED_LIBRARIES := angle_gles2
-LOCAL_SRC_FILES := tinywrapper/main.c tinywrapper/string_utils.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/tinywrapper
-include $(BUILD_SHARED_LIBRARY)
-
-$(call import-module,prefab/bytehook)
-LOCAL_PATH := $(HERE_PATH)
 
 include $(CLEAR_VARS)
 # Link GLESv2 for test
 LOCAL_LDLIBS := -ldl -llog -landroid
 # -lGLESv2
 LOCAL_MODULE := pojavexec
-LOCAL_SHARED_LIBRARIES := bytehook
 # LOCAL_CFLAGS += -DDEBUG
 # -DGLES_TEST
 LOCAL_SRC_FILES := \
@@ -43,7 +28,6 @@ LOCAL_SRC_FILES := \
     input_bridge_v3.c \
     jre_launcher.c \
     utils.c \
-    stdio_is.c \
     driver_helper/nsbypass.c
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
@@ -59,6 +43,16 @@ LOCAL_SRC_FILES := driver_helper/hook.c
 LOCAL_LDFLAGS := -z global
 include $(BUILD_SHARED_LIBRARY)
 #endif
+
+$(call import-module,prefab/bytehook)
+LOCAL_PATH := $(HERE_PATH)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := istdio
+LOCAL_SHARED_LIBRARIES := bytehook
+LOCAL_SRC_FILES := \
+    stdio_is.c
+include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pojavexec_awt
